648b103c321a3b3f010d5d2f1ea04631bea1fece
MartiRoll29 <141012622+MartiRoll29@users.noreply.github.com>
2026-07-08 22:49:24 -0600

    Cambios semana 3

---
+++ src/main/java/pruabTech/Rolando/repository/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package pruabTech.Rolando.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pruabTech.Rolando.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+    public List<Categoria> findByActivoTrue();
+}
